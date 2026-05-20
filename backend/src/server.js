495da10837b517d63ea1805dc70289478ebc2feb
Atharva Ludbe <atharvaludbe2@example.com>
2026-05-20 14:17:28 +0530

    khargosh

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,18 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
-connectDB();
 
 app.get("/", (req, res) => res.send("Hello from server"));
 
-app.listen(ENV.PORT, () =>
-    console.log(`Server is running on port ${ENV.PORT} ✅`)
-);
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
